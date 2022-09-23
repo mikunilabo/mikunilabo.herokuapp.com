63ab41a6c005e281507661eca9213f036b3a791d
和田邦康 <k-wada@mikunilabo.com>
2022-09-24 01:01:07 +0900

    グローバルDBサーバを利用した備考共有の仕組みを考案＆仮マイグレーション作成

---
+++ database/migrations/2022_09_23_000010_create_global_remarks_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * @var string
+     */
+    private $table = 'global_remarks';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        try {
+            Schema::create($this->table, function (Blueprint $table) {
+                $table->id();
+                $table->string('name')->nullable();
+                $table->string('key')->nullable();
+                $table->string('subject')->nullable();
+                $table->text('content')->nullable();
+                $table->timestamps();
+
+                $table->unique([
+                    'name',
+                    'key',
+                ]);
+            });
+        } catch (\Exception $e) {
+            report($e);
+            $this->down();
+            dd($e->getMessage());
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists($this->table);
+    }
+};
