144178a37bd8abe6f13efe2c68ccc0e4827f0c05
和田邦康 <k-wada@mikunilabo.com>
2022-09-24 00:59:44 +0900

    LINE WEBHOOK諸々調整

--- app/Listeners/Vendor/Line/Message/TextMessageListener.php
+++ app/Listeners/Vendor/Line/Message/TextMessageListener.php
@@ -7,6 +7,8 @@ use App\Services\Vendor\Line\Line;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use LINE\LINEBot\Event\MessageEvent\TextMessage;
+use LINE\LINEBot\MessageBuilder\RawMessageBuilder;
+use LINE\LINEBot\Response;
 
 class TextMessageListener
 {
@@ -14,6 +16,11 @@ class TextMessageListener
      * @var Line
      */
     private $line;
+
+    /**
+     * @var TextMessage
+     */
+    private $event;
 
     /**
      * @param Line $line
@@ -25,11 +32,6 @@ class TextMessageListener
     }
 
     /**
-     * @var TextMessage
-     */
-    private $event;
-
-    /**
      * @param  TextMessage  $event
      * @return void
      */
@@ -37,7 +39,7 @@ class TextMessageListener
     {
         $this->event = $event;
 
-        $response = $this->line->bot()->replyText($this->event->getReplyToken(), $this->message());
+        $response = $this->process();
 
         if (! $response->isSucceeded()) {
             logger()->error(static::class.$response->getHTTPStatus(), $response->getJSONDecodedBody());
@@ -45,17 +47,9 @@ class TextMessageListener
     }
 
     /**
-     * @return string
+     * @return Response
      */
-    private function message(): string
-    {
-        return $this->parseMessage();
-    }
-
-    /**
-     * @return string
-     */
-    private function parseMessage(): string
+    private function process(): Response
     {
         switch ($text = $this->event->getText()) {
             case '査定':
@@ -64,16 +58,31 @@ class TextMessageListener
             case '卸登録':
             case 'メッセージ':
             case 'お知らせ':
-                return sprintf('%sですね！現在未実装です。', $text);
+                return $this->line->bot()
+                    ->replyText($this->replyToken(), sprintf('%sですね！現在未実装です。', $text));
+
+            case '年齢':
+                return $this->line->bot()
+                    ->replyMessage($this->replyToken(), new RawMessageBuilder($this->agesQuickReply()));
+
             default:
-                return $this->randomReply();
+                return $this->line->bot()
+                    ->replyText($this->replyToken(), $this->randomMessage());
         }
     }
 
     /**
      * @return string
      */
-    private function randomReply(): string
+    private function replyToken(): string
+    {
+        return $this->event->getReplyToken();
+    }
+
+    /**
+     * @return string
+     */
+    private function randomMessage(): string
     {
         switch (mt_rand(0, 2)) {
             case 0:
@@ -85,5 +94,39 @@ class TextMessageListener
             default:
                 return 'すみません、AIがリプライを思い付かなかったです..💦';
         }
+    }
+
+    /**
+     * @return array
+     */
+    public function agesQuickReply(): array
+    {
+        return [
+            'type' => 'text',
+            'text' => 'あなたの年齢を教えてください。',
+            'quickReply' => [
+                'items' => $this->agesArray(),
+            ]
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function agesArray(): array
+    {
+        $arr = [];
+        for ($i=10; $i<60; $i+=10) {
+            $arr[] = [
+                'type' => 'action',
+                'action' => [
+                    'type' => 'message',
+                    'label' => $label = sprintf('%s代', $i),
+                    'text' => $label,// アクションの実行時に送信されるテキスト
+                    //ここへ次の回答時にフローを繋げるための一意のIDを付加しておく必要あると思っていたが、トーク画面で出力されてしまうので、要再考慮...
+                ],
+            ];
+        }
+        return $arr;
     }
 }
